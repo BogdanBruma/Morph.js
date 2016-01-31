6bb47d3f8f6d57dbcb45f3f11927cb651ab51173
BogdanBruma <brumabogdan95@yahoo.com>
2016-01-31 19:56:40 +0200

    added morph concept

---
+++ morph.js
@@ -0,0 +1,27 @@
+function Morph() {
+    var morphs = [].slice.call(arguments);
+
+    return function() {
+        var argv = [].slice.call(arguments),
+            matchedMorphs = morphs.filter(function(morph) {
+                var args = morph.slice(0, morph.length - 1);
+
+                return args.length === argv.length &&
+                	args.every(function(checks, i) {
+                        return checks.every(function(check) {
+                            return check.call(check, argv[i]);
+                        });
+                    });
+            });
+
+        if (matchedMorphs.length === 0) {
+            throw new Error('No morph case found.');
+        }
+
+        if (matchedMorphs.length !== 1) {
+            throw new Error('Multiple morph cases found.');
+        }
+
+        return matchedMorphs[0][matchedMorphs[0].length - 1].apply(this, argv);
+    };
+}
